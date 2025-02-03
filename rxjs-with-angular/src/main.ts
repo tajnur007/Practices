69914156fb4df05178391a0edabc2797579add29
Kazi Tajnur Islam <tajnur007@gmail.com>
2025-02-03 16:13:12 +0600

    [rxjs-with-angular]: Remove unused codes

--- rxjs-with-angular/src/main.ts
+++ rxjs-with-angular/src/main.ts
@@ -1,13 +1,4 @@
-import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
+import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
-
 platformBrowser().bootstrapModule(AppModule).catch((err) => console.error(err));
-
-// bootstrapApplication(AppModule)
-//   .catch((err) => console.error(err));
-
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
